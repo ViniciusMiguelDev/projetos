2cdb3e72fc4dbeb62a4f1fd0130e3fb537a67d29
Vinicius Miguel <vmfasdev@gmail.com>
2024-08-16 16:37:58 -0300

    portfolio att (16/08/2024)

--- portifolio/js/script.js
+++ portifolio/js/script.js
@@ -15,17 +15,3 @@ function toggleMenu () {
         nav.style.display = 'none';
     }
 };
-
-document.getElementById('read-more').addEventListener('click', function(event) {
-    event.preventDefault();  // Evita o comportamento padrão do link
-    const moreText = document.querySelector('.more-text');
-    const btnText = document.getElementById('read-more');
-
-    if (moreText.style.display === 'none') {
-        moreText.style.display = 'inline';
-        btnText.textContent = 'Ler Menos';
-    } else {
-        moreText.style.display = 'none';
-        btnText.textContent = 'Ler Mais';
-    }
-});
